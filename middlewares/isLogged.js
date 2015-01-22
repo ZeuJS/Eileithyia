62b93e1b95bda2229970f426d509576c40006803
Gaël Demette <demette.gael@gmail.com>
2015-01-22 01:31:22 +0100

    Most simple

--- middlewares/isLogged.js
+++ middlewares/isLogged.js
@@ -2,12 +2,10 @@
 var bcrypt = require('bcrypt');
 
 module.exports = function (rails, pass) {
-  if (!rails.identity) {
-    pass(false, function() {
-      rails.response.json({
-        success: false,
-        message: 'Identity is required',
-      }, 403)
-    });
-  }
+  pass(!!rails.identity, function() {
+    rails.response.json({
+      success: false,
+      message: 'Identity is required',
+    }, 403)
+  });
 };
